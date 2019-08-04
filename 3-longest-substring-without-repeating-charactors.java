ab38819542b3b57b61cec9ccec04c0671906700e
hthuang <hthuang@chinaums.com>
2019-08-04 14:22:03 +0800

    3-longest-substring-without-repeating-charactors

--- 3-longest-substring-without-repeating-charactors.java
+++ 3-longest-substring-without-repeating-charactors.java
@@ -1,18 +1,18 @@
 /**
  * Given a string,find the length of the longest substring without repeating characters.
- * <p>
+ * 
  * Example 1:
- * <p>
+ * 
  * Input:"abcabcbb"
  * Output:3
  * Explanation:The answer is"abc",with the length of 3.
  * Example 2:
- * <p>
+ * 
  * Input:"bbbbb"
  * Output:1
  * Explanation:The answer is"b",with the length of 1.
  * Example 3:
- * <p>
+ * 
  * Input:"pwwkew"
  * Output:3
  * Explanation:The answer is"wke",with the length of 3.
